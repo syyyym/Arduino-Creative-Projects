049a1bb949bd80f042b263f19031021c06fb0e2f
syyyym <gabi.lupusor@gmail.com>
2020-02-24 17:39:15 +0200

    reset pins after each loop

--- 1All-colors-changing-light/1All-colors-changing-light.ino
+++ 1All-colors-changing-light/1All-colors-changing-light.ino
@@ -19,9 +19,9 @@ int blueValue;
 
 void loop()
 {
-#define delayTime 10 // fading time between colors
+  #define delayTime 10
 
-  redValue = 255; // choose a value between 1 and 255 to change the color.
+  redValue = 255;
   greenValue = 0;
   blueValue = 0;
 
@@ -30,17 +30,20 @@ void loop()
   {
     redValue -= 1;
     greenValue += 1;
-
     analogWrite(RED, redValue);
     analogWrite(GREEN, greenValue);
     delay(delayTime);
   }
 
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+  delay(300);
+
   redValue = 0;
   greenValue = 255;
   blueValue = 0;
 
-  // fades out green bring blue full
+  // fade out green bring blue full
   for (int i = 0; i < 255; i += 1)
   {
     greenValue -= 1;
@@ -51,11 +54,15 @@ void loop()
     delay(delayTime);
   }
 
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, LOW);
+  delay(300);
+
   redValue = 0;
   greenValue = 0;
   blueValue = 255;
 
-  // fades out blue bring red full when i=255
+  // fade out blue bring red full
   for (int i = 0; i < 255; i += 1)
   {
     blueValue -= 1;
@@ -65,4 +72,8 @@ void loop()
     analogWrite(RED, redValue);
     delay(delayTime);
   }
+
+  digitalWrite(BLUE, LOW);
+  digitalWrite(RED, LOW);
+  delay(300);
 }
