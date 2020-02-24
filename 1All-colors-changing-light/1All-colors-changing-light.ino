b6383a79b25e20833dfd68bbe16351be9c7d54f3
syyyym <gabi.lupusor@gmail.com>
2020-02-24 17:30:12 +0200

    define the pins for each color, set pins as outputs

---
+++ 1All-colors-changing-light/1All-colors-changing-light.ino
@@ -0,0 +1,19 @@
+// Define Pins
+#define BLUE 3
+#define GREEN 5
+#define RED 6
+
+void setup()
+{
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, LOW);
+}
+
+void loop()
+{
+
+}
