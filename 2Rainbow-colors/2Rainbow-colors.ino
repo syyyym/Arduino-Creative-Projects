72bb45df2a8e7038225bee0c3f0e861bdf02f194
syyyym <gabi.lupusor@gmail.com>
2020-03-02 15:46:44 +0200

    define the pins for the chip: latch, clock and data pins

---
+++ 2Rainbow-colors/2Rainbow-colors.ino
@@ -0,0 +1,18 @@
+int tDelay = 1000;
+int latchPin = 11;
+int clockPin = 9;
+int dataPin = 12;
+
+byte leds = 0;
+
+void setup() 
+{
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
+  pinMode(clockPin, OUTPUT);
+}
+
+void loop() 
+{
+  
+}
