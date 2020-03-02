67d59782d17cce2431d4bbedf4af0181c2e8f355
syyyym <gabi.lupusor@gmail.com>
2020-03-02 16:08:54 +0200

    add the code from Rainbow colors project

---
+++ 3Light-sensitive-colors/3Light-sensitive-colors.ino
@@ -0,0 +1,33 @@
+int tDelay = 1000;
+int latchPin = 11;
+int clockPin = 9;
+int dataPin = 12;
+
+byte leds = 0; //pattern of the 7 leds
+
+void updateShiftRegister()
+{
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, leds); //LSB - least significant bit
+  digitalWrite(latchPin, HIGH);
+}
+
+void setup()
+{
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+}
+
+void loop()
+{
+  leds = 0;
+  updateShiftRegister();
+  delay(tDelay);
+  for (int i = 0; i < 8; i++)
+  {
+    bitSet(leds, i);
+    updateShiftRegister();
+    delay(tDelay);
+  }
+}
