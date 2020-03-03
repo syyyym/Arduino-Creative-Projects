1576cbcb8bd62a6fe48053ad364051d614ff004b
syyyym <gabi.lupusor@gmail.com>
2020-03-03 19:12:42 +0200

    play each note frequency according to the voltge range of the button pressed

--- 5Keyboard-instrument/5Keyboard-instrument.ino
+++ 5Keyboard-instrument/5Keyboard-instrument.ino
@@ -10,4 +10,20 @@ void loop()
   int keyVal = analogRead(A0);
 
   Serial.println(keyVal);
+
+  if (keyVal == 1023) {
+    tone(8, notes[0]);
+  }
+  else if (keyVal >= 990 && keyVal <= 1010) {
+    tone(8, notes[1]);
+  }
+  else if (keyVal >= 505 && keyVal <= 525) {
+    tone(8, notes[2]);
+  }
+  else if (keyVal >= 8 && keyVal <= 50) {
+    tone(8, notes[3]);
+  }
+  else {
+    noTone(8); //no button pressed
+  }
 } 
